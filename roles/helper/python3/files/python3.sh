abd1eb3b6fce154644c835bfd014709a9e55cc3f
Álvaro Curto Merino <alvaro.curtomerino@telefonica.com>
2025-05-16 14:33:41 +0200

    If python helper is the first command a host runs, it needs to update its caché

--- roles/helper/python3/files/python3.sh
+++ roles/helper/python3/files/python3.sh
@@ -4,11 +4,13 @@ set -o errexit
 set -x
 
 if [[ -f /etc/debian_version ]]; then
+    DEBIAN_FRONTEND=noninteractive apt-get -q update
     DEBIAN_FRONTEND=noninteractive apt-get -q install -y python3
     exit
 fi
 
 if [[ -f /etc/redhat-release ]]; then
+    dnf -q update
     dnf -q install -y python3
     exit
 fi
