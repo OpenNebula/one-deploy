4578818ca9bd9a9ead0f6d6358a204a2360d4609
Michal Opala <sk4zuzu@gmail.com>
2026-03-25 13:21:52 +0100

    M #-: Allow installation of ruamel.yaml via pip
    - Remove hardcoded ansible_python_interpreter from group_vars
    - Add missing allow_duplicates meta setting (fix)
    - Introduce generic way to install python3 packages via pip
    - Auto-install pip for custom intepreters when needed (when unconfigured)
    - Auto-install ruamel.yaml via pip for custom intepreters (when unconfigured)
    - Anticipate SLES15 support
    
    Signed-off-by: Michal Opala <sk4zuzu@gmail.com>

--- roles/helper/python3/files/python3.sh
+++ roles/helper/python3/files/python3.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+: "${PYTHON3:=python3}"
+: "${PYTHON3_NO_DOTS:=${PYTHON3//.}}"
+
 set -o errexit
 set -x
 
 if [[ -f /etc/debian_version ]]; then
-    DEBIAN_FRONTEND=noninteractive apt-get -q install -y python3
+    DEBIAN_FRONTEND=noninteractive apt-get -q install -y "$PYTHON3"
     exit
 fi
 
 if [[ -f /etc/redhat-release ]]; then
-    dnf -q install -y python3
+    dnf -q install -y "$PYTHON3"
     exit
 fi
 
 if [[ -f /etc/SUSE-brand ]]; then
-    zypper --non-interactive install -y python3
+    zypper --non-interactive install -y "$PYTHON3_NO_DOTS"
     exit
 fi
