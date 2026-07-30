ec006455b8d967036d34d973a6cd9886a309e3dd
Michal Opala <sk4zuzu@gmail.com>
2026-07-30 23:44:04 +0200

    M #-: Apply several fixes
    - Silently skip invalid hostnames (fix)
    - Rename debounce to throttle (fix)
    - Make sure throttle synchronizes correctly (fix)
    - Do not truncate(0) /etc/hosts (fix)
    - Do not refresh /etc/hosts on followers (fix)
    - Set correct permissions for resolved's drop-in (fix)
    
    Signed-off-by: Michal Opala <sk4zuzu@gmail.com>

--- roles/helper/vmdns/files/refresh.rb
+++ roles/helper/vmdns/files/refresh.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/env ruby
 # managed by one-deploy
 
+RFC1123_REGEX = %r{ \A
+
+                    (?=.{1,253}\z)
+
+                    (?:
+                        [a-zA-Z0-9]
+                        (?:
+                            [a-zA-Z0-9-]{0,61}
+                            [a-zA-Z0-9]
+                        )?
+                        [.]
+                    )*
+
+                    [a-zA-Z0-9]
+                    (?:
+                        [a-zA-Z0-9-]{0,61}
+                        [a-zA-Z0-9]
+                    )?
+
+                    \z }x
+
 def run(cmd)
     o = `#{cmd}`
     s = $?
@@ -8,21 +29,25 @@ def run(cmd)
     o
 end
 
-def debounce(lock_file = '/var/tmp/vmdns.lock')
-    File.open(lock_file, File::CREAT | File::RDWR, 0o0644) do |lf|
-        next unless lf.flock(File::LOCK_EX | File::LOCK_NB)
-        begin
-            # NOTE: This is not completely precise, but should be good enough
-            #       for a simple reduction.
-            sleep(1)
-            yield
-        ensure
-            lf.flock(File::LOCK_UN)
-        end
+def throttle(thrt_lock = '/var/tmp/vmdns-thrt.lock',
+             exec_lock = '/var/tmp/vmdns-exec.lock')
+    # Silently drop rapid bursts.
+    File.open(thrt_lock, File::CREAT | File::RDWR, 0o0644) do |lf|
+        return unless lf.flock(File::LOCK_EX | File::LOCK_NB)
+        sleep(1)
+    end
+    # Ensure exclusive execution.
+    File.open(exec_lock, File::CREAT | File::RDWR, 0o0644) do |lf|
+        lf.flock(File::LOCK_EX)
+        yield
     end
 end
 
 def refresh(hosts_file = '/etc/hosts')
+    require 'json'
+
+    document = JSON.parse run('onevm list --json')
+
     File.open(hosts_file, File::RDONLY) do |hf|
         hf.each_line(chomp: true).each_with_object({block: false, lines: []}) do |line, acc|
             case line
@@ -35,33 +60,37 @@ def refresh(hosts_file = '/etc/hosts')
             end
         end[:lines]
     end.then do |unmanaged|
-        require 'json'
+        document.dig('VM_POOL', 'VM')&.each_with_object([]) do |vm, acc|
+            ip = [vm.dig('TEMPLATE', 'NIC')].flatten.dig(0, 'IP')
+            next unless ip
 
-        document = JSON.parse run('onevm list --json')
+            name = vm['NAME']
+            next unless name && name =~ RFC1123_REGEX
 
-        File.open(hosts_file, File::CREAT | File::TRUNC | File::WRONLY, 0o0644) do |hf|
-            unmanaged.each do |line|
-                hf.puts(line)
+            id = vm['ID']
+            next unless id
+
+            acc << "#{ip} #{name} one-#{id}"
+        end.then do |managed|
+            File.open(hosts_file, File::CREAT | File::RDWR, 0o0644) do |hf|
+                hf.rewind
+
+                unmanaged.each { |line| hf.puts(line) }
+
+                # NOTE: The original / unmanaged data is never completely removed
+                #       from the file.
+                hf.truncate(hf.pos)
+
+                unless managed.empty?
+                    hf.puts('# BEGIN VMDNS (one-deploy)')
+
+                    managed.each { |line| hf.puts(line) }
+
+                    hf.puts('# END VMDNS (one-deploy)')
+                end
             end
-
-            hf.puts('# BEGIN VMDNS (one-deploy)')
-
-            document.dig('VM_POOL', 'VM')&.each do |vm|
-                ip = [vm.dig('TEMPLATE', 'NIC')].flatten.dig(0, 'IP')
-                next unless ip
-
-                name = vm['NAME']
-                next unless name
-
-                id = vm['ID']
-                next unless id
-
-                hf.puts("#{ip} #{name} one-#{id}")
-            end
-
-            hf.puts('# END VMDNS (one-deploy)')
         end
     end
 end
 
-debounce { refresh }
+throttle { refresh } if [nil, 'leader'].include?(ARGV[0])
