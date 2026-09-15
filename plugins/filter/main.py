1c7a838e603b0758e450647e4e78cc4f7f5503dc
Jan Orel <jorel@opennebula.io>
2026-09-15 16:35:31 +0200

    F #257: Add oned_conf/kvm_conf/kvmrc inventory overrides (#291)

--- plugins/filter/main.py
+++ plugins/filter/main.py
@@ -1,7 +1,7 @@
-from ansible_collections.opennebula.deploy.plugins.module_utils.main import to_one
+from ansible_collections.opennebula.deploy.plugins.module_utils.main import to_one, to_actions
 
 
 class FilterModule(object):
 
     def filters(self):
-        return dict(to_one=to_one)
+        return dict(to_one=to_one, to_actions=to_actions)
